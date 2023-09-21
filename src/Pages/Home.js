1390acd21d67c539a84efa4937bb32af46edc644
Pramo-d <promody1444@gmail.com>
2023-09-21 17:58:42 +0530

    api call and loading method

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import { useState} from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h2>Home page</h2>
-    </div>
-  )
-}
+  const [movie, setMovie] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Home
+  const fetchApiData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+
+      setMovie(data.results);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={fetchApiData}>fetch Movies</button>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {movie.map((movieData, index) => (
+            <li key={index}>
+              <h2>{movieData.title}</h2>
+              <p>Episode {movieData.episode_id}</p>
+              <p>{movieData.opening_crawl}</p>
+              <p>Release Date: {movieData.release_date}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Home;
